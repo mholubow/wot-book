f3ad249532aac2ffa933ec1e5d41208b4ef825ba
mholubow <holubowicz50@gmail.com>
2019-09-27 22:24:09 +0200

    modified:   dht22/server.js

--- dht22/server.js
+++ dht22/server.js
@@ -2,7 +2,7 @@ const sensor = require("node-dht-sensor").promises;
  
 async function exec() {
   try {
-    const res = await sensor.read(22, 4);
+    const res = await sensor.read(22, 17);
     console.log(
       `temp: ${res.temperature.toFixed(1)}°C, ` +
         `humidity: ${res.humidity.toFixed(1)}%`
